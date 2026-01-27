fd14baa178091a79dc152f197fd87ade3cfd918b
ZaidHani <allwazaid1@gmail.com>
2026-01-27 07:53:44 +0300

    not real changes, just fixed the docker compose file

--- src/init-multiple-dbs.sh
+++ src/init-multiple-dbs.sh
@@ -5,3 +5,11 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     CREATE DATABASE mlflow;
     GRANT ALL PRIVILEGES ON DATABASE mlflow TO mlflow;
 EOSQL
+
+# Connect to the mlflow database and grant schema permissions
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "mlflow" <<-EOSQL
+    GRANT ALL ON SCHEMA public TO mlflow;
+    GRANT CREATE ON SCHEMA public TO mlflow;
+    ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO mlflow;
+    ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO mlflow;
+EOSQL
